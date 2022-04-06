64c984672f937c325c3531e4320bb031274f66bb
muhammed nur <muhammednurekin@gmail.com>
2022-04-07 01:33:31 +0300

    newer version 9

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/transaction-matcher'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/transaction-matcher/index.html'));});
+//((app.use(express.static(__dirname + '/dist/transaction-matcher'));
+//app.get('/*', function(req,res) {
+//res.sendFile(path.join(__dirname+
+//'/dist/transaction-matcher/index.html'));});
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
